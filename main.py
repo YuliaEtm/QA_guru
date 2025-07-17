b1647b5b93e3f32b4644a4667e405a07586983e8
Julia Etmanova <blikdom@yandex.ru>
2025-07-17 21:39:24 +0300

    все работает опять

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/api/users/2")
+# @app.get("/api/users/10")
+# def get_user():
+#     return {
+#         "data": {
+#             "id": 10,
+#             "email": "byron.fields@reqres.in",
+#             "first_name": "Byron",
+#             "last_name": "Fields",
+#             "avatar": "https://reqres.in/img/faces/10-image.jpg"
+#         },
+#         "support": {
+#             "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+#             "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
+#         }
+#     }
+@app.get("/api/unknown")
 def get_user():
-    return {
-        "data": {
-            "id": 2,
-            "email": "janet.weaver@reqres.in",
-            "first_name": "Janet",
-            "last_name": "Weaver",
-            "avatar": "https://reqres.in/img/faces/2-image.jpg"
-        },
-        "support": {
-            "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-            "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
-        }
-    }
+    return {"page": 1, "per_page": 6, "total": 12, "total_pages": 2,
+            "data": [{"id": 1, "name": "cerulean", "year": 2000, "color": "#98B2D1", "pantone_value": "15-4020"},
+                     {"id": 2, "name": "fuchsia rose", "year": 2001, "color": "#C74375", "pantone_value": "17-2031"},
+                     {"id": 3, "name": "true red", "year": 2002, "color": "#BF1932", "pantone_value": "19-1664"},
+                     {"id": 4, "name": "aqua sky", "year": 2003, "color": "#7BC4C4", "pantone_value": "14-4811"},
+                     {"id": 5, "name": "tigerlily", "year": 2004, "color": "#E2583E", "pantone_value": "17-1456"},
+                     {"id": 6, "name": "blue turquoise", "year": 2005, "color": "#53B0AE", "pantone_value": "15-5217"}],
+            "support": {"url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+                        "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."}}
 
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
     # uvicorn main:app --reload
--- test_users.py
+++ test_users.py
@@ -1,16 +1,27 @@
 import requests
 
 
-def test_user_data():
-    # url = "https://reqres.in/api/users/2"
-    url = " http://127.0.0.1:8000/api/users/2"
-    headers = {'x-api-key': 'reqres-free-v1'}
-    expected_id = 2
-    expected_email = "janet.weaver@reqres.in"
+# def test_get_single_user_micro():
+#     # url = "https://reqres.in/api/users/2"
+#     url = " http://127.0.0.1:8000/api/users/10"
+#     headers = {'x-api-key': 'reqres-free-v1'}
+#     expected_id = 10
+#     expected_email = "janet.weaver@reqres.in"
+#
+#     response = requests.get(url, headers=headers)
+#     print(response.text)
+#     body = response.json()
+#     data = body["data"]
+#
+#     assert data["id"] == expected_id
 
-    response = requests.get(url, headers=headers)
+
+def test_get_users_returns_unique_users_micro():
+    # url = "https://reqres.in/api/unknown"
+    url = "http://127.0.0.1:8000/api/unknown"
+    headers = {'x-api-key': 'reqres-free-v1'}
+    response = requests.get(url=url, headers=headers)
+    assert response.status_code == 200
     print(response.text)
     body = response.json()
-    data = body["data"]
-
-    assert data["id"] == expected_id
+    assert body['per_page'] == len(body['data'])
