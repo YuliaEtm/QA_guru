d0e8e50e78299c6e7e9344beb3aac6f491e3a196
Julia Etmanova <blikdom@yandex.ru>
2025-08-05 21:41:12 +0300

    урок4

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,15 +1,1 @@
-from http import HTTPStatus
-import pytest
-import requests
-from models.User import User
-
-
-def test_status_ok(app_url):
-    response = requests.get(f"{app_url}/status")
-    assert response.status_code == HTTPStatus.OK
-
-
-def test_status_true(app_url):
-    response = requests.get(f"{app_url}/status").json()
-    assert response['users'] is True, 'данные не загружены'
 
