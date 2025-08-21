92b3dcf1ee493b2875c97517d82546dd0811c066
Julia Etmanova <blikdom@yandex.ru>
2025-08-21 08:56:03 +0300

    работает через main

--- http_client.py
+++ http_client.py
@@ -9,14 +9,15 @@ class HTTPClient:
         self.base_url = base_url
         self.headers = headers
 
-    def _logger_request(self, method, path, **kwargs):
+    def _logger_request(self, method, path, headers, **kwargs):
         request = f"""
         method: (method)
         path: (path)
         kwargs: (kwargs)
+        
         """
         print(request)
-        response = self.session.request(method, self.base_url+path, **kwargs)
+        response = self.session.request(method, self.base_url+path, headers, **kwargs)
         response_massage = f"""
         status_code: {response.status_code}
         json: {pprint.pformat(response.json())}
@@ -24,3 +25,9 @@ class HTTPClient:
         print(response_massage)
 
         return response
+
+    def get(self, path, headers, **kwargs):
+        return self._logger_request("GET", path, self.headers,  **kwargs)
+
+    def post(self, path, headers, **kwargs):
+        return self._logger_request("POST", path, self.headers, **kwargs)
--- regisrer_api.py
+++ regisrer_api.py
@@ -3,31 +3,33 @@ from symtable import Class
 
 import requests
 
+from http_client import HTTPClient
+
+
 class RegisterApi:
-    def __init__(self):
-        self.base_url = "https://reqres.in"
-        # BASE_URL = "https://reqres.in"
+    def __init__(self, http_client: HTTPClient):
+        self.http_client = http_client
         self.headers = {'x-api-key': 'reqres-free-v1'}
 
+    def check_user(self, headers):
+        response = self.http_client.get(path='/api/users/2', headers=headers)
+        assert response.status_code == 200
+        body = response.json()['data']
+        print(body)
+        assert body['first_name'] == "Janet"
+        return response
 
-def check_user(self):
-    response = requests.get(f"{BASE_URL}/api/users/2", headers=headers)
-    assert response.status_code == 200
-    body = response.json()['data']
-    print(body)
-    assert body['first_name'] == "Janet"
+    def create_user(self, headers):
+        payload = {
+            "name": "morpheus",
+            "job": "leader"
+        }
+        response = self.http_client.post(path="/api/users", json=payload, headers={'x-api-key': 'reqres-free-v1'})
+        print(response.text)
+        # assert response.status_code in (200, 201)
+        return response
 
-
-def create_user(self):
-    payload = {
-        "name": "morpheus",
-        "job": "leader"
-    }
-    response = requests.post(f"{BASE_URL}/api/users", json=payload, headers=headers)
-    print(response.text)
-    assert response.status_code in (200, 201)
-
-    # {"data":{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver",
-    # "avatar":"https://reqres.in/img/faces/2-image.jpg"},
-    # "support":{"url":"https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-    # "text":"Tired of writing endless social media content? Let Content Caddy generate it for you."}}
+        # {"data":{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver",
+        # "avatar":"https://reqres.in/img/faces/2-image.jpg"},
+        # "support":{"url":"https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+        # "text":"Tired of writing endless social media content? Let Content Caddy generate it for you."}}
